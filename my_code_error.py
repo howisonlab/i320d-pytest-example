0366eb5a86870a56a75f29a0f380749dc9092147
Daniel Chayet <dchayet@utexas.edu>
2024-04-10 16:28:05 -0500

    revert my_code_error.py

--- my_code_error.py
+++ my_code_error.py
@@ -5,9 +5,6 @@ def fix_phone_num(phone_num_to_fix):
   if (len(phone_num_to_fix) != 10):
     raise ValueError("Can only format numbers that are exactly 10 digits long")
 
-  if not phone_num_to_fix.isdigit():
-      raise ValueError("phone number must contain only digits")
-
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
@@ -19,24 +16,7 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
-  assert fix_phone_num("5554429876") == '(555) 442 9876'
-  assert fix_phone_num("3216543333") == '(321) 654 3333'
-
+  
   # Now check that a too short string gives a ValueError
   with pytest.raises(ValueError):
     fix_phone_num("51")
-
-def test_fixed_nums_not_handled():
-  assert fix_phone_num('555-442-98761') == '(555) 442 98761'
-  assert fix_phone_num('(321) 654 3333') == '(321) 654 3333'
-
-# def test_fixed_nums_handled():
-#   with pytest.raises(ValueError):
-#       fix_phone_num("555-442-98761")
-
-#   with pytest.raises(ValueError):
-#     fix_phone_num("(321) 654 3333")
-
-def test_is_digit():
-    with pytest.raises(ValueError):
-        fix_phone_num('not-numbers')
