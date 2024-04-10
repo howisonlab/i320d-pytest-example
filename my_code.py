221253ab82b63fe1df809dc39f3e68cfbc60625f
RiyaPottapinjara <145230449+RiyaPottapinjara@users.noreply.github.com>
2024-04-10 16:39:47 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -11,11 +11,11 @@ def fix_phone_num(phone_num_to_fix):
   return fixed_num
 
 def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8823'
-  assert fix_phone_num("5554429876") == '(555) 442 9876'
-  assert fix_phone_num("3216543333") == '(321) 654 3333'
+  assert fix_phone_num("5125558823") == '(512) 521 8823'
+  assert fix_phone_num("5554429876") == '(555) 421 9876'
+  assert fix_phone_num("3216543333") == '(321) 651 3333'
   
 def fix_dash_paranthesis():
-  assert fix_phone_num("555-442-98761") = '(555) 442 9876'
-  assert fix_phone_num("(321) 654 3333") = '(321) 654 3333'
+  assert fix_phone_num("555-442-98761") = '(555) 422 9876'
+  assert fix_phone_num("(321) 654 3333") = '(321) 654 3321'
 
